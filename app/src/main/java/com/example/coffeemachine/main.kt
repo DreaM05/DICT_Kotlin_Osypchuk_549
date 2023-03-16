0e796f42c6dc35f3b78f54672bdad480b472c727
Aleksey <bibliotekar473@gmail.com>
2023-03-17 00:43:00 +0200

    Coffee Machine Project - Stage 2

--- app/src/main/java/com/example/coffeemachine/main.kt
+++ app/src/main/java/com/example/coffeemachine/main.kt
@@ -4,4 +4,13 @@ import java.util.*
 
 fun main() {
     println("Starting to make a coffee in NIX office\n" + "Grinding coffee beans\n" + "Boiling water\n" + "Mixing boiled water with crushed coffee beans\n" + "Pouring coffee into the cup\n" + "Pouring some milk into the cup\n" + "Coffee is ready! Go to work!")
+    val scan = java.util.Scanner(System.`in`)
+
+    val water = 200
+    val milk = 50
+    val coffee_beans = 15
+    println("Write how many cups of coffee you will need:")
+    val cup = scan.nextInt()
+    println("For " + cup + " cups of coffee you will need: \n" + (water * cup) + " ml of water\n" + (milk * cup) + " ml of milk\n" + (coffee_beans * cup) + " g of coffee beans\n")
+
 }
