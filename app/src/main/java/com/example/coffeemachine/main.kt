f89ed0f2bf1ff91dfe4b9bf0b3fd50865c2d07b5
Aleksey <bibliotekar473@gmail.com>
2023-03-17 00:39:38 +0200

    Coffee Machine Project - Stage 1

---
+++ app/src/main/java/com/example/coffeemachine/main.kt
@@ -0,0 +1,7 @@
+package com.example.coffeemachine
+
+import java.util.*
+
+fun main() {
+    println("Starting to make a coffee in NIX office\n" + "Grinding coffee beans\n" + "Boiling water\n" + "Mixing boiled water with crushed coffee beans\n" + "Pouring coffee into the cup\n" + "Pouring some milk into the cup\n" + "Coffee is ready! Go to work!")
+}
